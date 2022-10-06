f0570b3bc0204dddfc23ec8db7a94762ff0ac04d
Manuel Benini <manuelbenini1905@gmail.com>
2022-10-06 14:05:53 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzasController.cs
@@ -0,0 +1,71 @@
+﻿using la_mia_pizzeria_post.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics;
+
+namespace la_mia_pizzeria_post.Controllers
+{
+    public class PizzasController : Controller
+    {
+
+        public List<Pizza> Pizzas()
+        {
+            using PizzaContext db = new();
+            {
+                List<Pizza> pizzas = db.Pizzas.ToList<Pizza>();
+
+                //Se la tabella Pizzas del DB è vuota, aggiungo delle pizze d'esempio
+                if (pizzas.Count == 0)
+                {
+                    db.Add(new Pizza("Margherita", "La pizza Margherita", "https://www.finedininglovers.it/sites/g/files/xknfdk1106/files/styles/recipes_1200_800_fallback/public/fdl_content_import_it/margherita-50kalo.jpg?itok=v9nHxNMS", 10.50));
+                    db.Add(new Pizza("Napoli", "La pizza Napoli", "https://media-cdn.tripadvisor.com/media/photo-s/18/03/98/d6/received-665664433902722.jpg", 14.50));
+                    db.Add(new Pizza("Romana", "La pizza Romana", "https://recipesblob.oetker.com/files/95bdfe7334364b41b557c734cd1c64c4/889e39b112414a9aa2b3ae5a9f787f6b/1272x764/pizza-alla-romanajpg.jpg", 17.50));
+                    db.Add(new Pizza("4 Gusti", "La pizza 4 Gusti", "https://media-cdn.tripadvisor.com/media/photo-s/07/61/12/f1/pizza-4-gusti.jpg", 5.50));
+                    db.SaveChanges();
+                }
+                
+                return pizzas;
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View(Pizzas());
+        }
+        public IActionResult Show(int id)
+        {
+            using PizzaContext db = new();
+            {
+                Pizza pizza = db.Pizzas.Where(pizzas => pizzas.PizzaId == id).First<Pizza>();
+                return View(pizza);
+            }
+            
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", model);
+            }
+            //Utility.Aggiungi(new Pizza(model.Name, model.Description, model.Image, model.Price));
+
+            using PizzaContext db = new();
+            {
+                db.Add(model);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_post.Models
+{
+    [Table("Pizzas")]
+    public class Pizza
+    {
+        public int PizzaId { get; set; }
+
+        [Required(ErrorMessage = "Il campo nome è obbligatorio")]
+        [StringLength(50, ErrorMessage = "Il nome non può avere più di 50 caratteri")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Il campo descrizione è obbligatorio")]
+        [MoreThanFiveWordsValidation]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Il campo immagine è obbligatorio")]
+        [Url(ErrorMessage = "L'immagine inserita non è valida")]
+        public string Image { get; set; }
+
+        [Required(ErrorMessage = "Il campo prezzo è obbligatorio")]
+        [MoreThanZeroValidation]
+        public double Price { get; set; }
+
+        public Pizza()
+        {
+
+        }
+
+        public Pizza(string name, string description, string image, double price)
+        {
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = price;
+        }
+    }
+}
